89e378e6fb607bd81ca96869d1d7683d0d08920a
Ole A. Sjo Fasting <oasf@kaliyuga.io>
2024-09-15 10:17:37 +0200

    added golang set env to asdf zsh stage

--- modules/zsh/zshrc/00-asdf.sh
+++ modules/zsh/zshrc/00-asdf.sh
@@ -1,5 +1,8 @@
-if [[ -e "$HOME" ]] && [[ -f "$HOME/.asdf/asdf.sh" ]]; then
-    source "$HOME/.asdf/asdf.sh"
-    fpath=(${ASDF_DIR}/completions $fpath)
-    autoload -Uz compinit && compinit
+if [[ -e "$HOME/.asdf" ]]; then
+    if [[ -e "$HOME/.asdf/asdf.sh" ]]; then
+        source "$HOME/.asdf/asdf.sh"
+        fpath=(${ASDF_DIR}/completions $fpath)
+        autoload -Uz compinit && compinit
+    fi
+    [[ -e "$HOME/.asdf/plugins/golang/set-env.zsh" ]] && source "$HOME/.asdf/plugins/golang/set-env.zsh"
 fi
